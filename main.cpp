6944bb1de3d9c61c9a9520d54136ef219f3a0e2b
Aliza <alirezanajmabadi79@gmail.com>
2025-12-15 16:02:53 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,11 @@
-
 #include <jni.h>
 #include <string>
-#include <zygisk.hpp>
+#include "zygisk.hpp"
 #include <android/log.h>
-#include <unordered_set>
-#include <dlfcn.h>
 
 #define LOG_TAG "GPU830Spoof"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-// پکیج هدف
 static const char* TARGET_PACKAGE = "flar2.devcheck";
 
 class GPU830Module : public zygisk::ModuleBase {
@@ -18,7 +13,6 @@ public:
     void onLoad(zygisk::Api* api, JNIEnv* env) override {
         this->api = api;
         this->env = env;
-        LOGI("GPU 830 Spoof Module loaded");
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs* args) override {
@@ -28,30 +22,36 @@ public:
         }
         
         const char* package_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        bool is_target = false;
         
-        if (package_name) {
-            LOGI("Checking package: %s", package_name);
-            is_target = (strcmp(package_name, TARGET_PACKAGE) == 0);
-            env->ReleaseStringUTFChars(args->nice_name, package_name);
-        }
-        
-        if (is_target) {
-            LOGI("Target package found! Setting up GPU 830 spoof...");
+        if (package_name && strcmp(package_name, TARGET_PACKAGE) == 0) {
+            LOGI("GPU 830 spoof activated for: %s", package_name);
             
             // اجرای اسپوف از طریق companion
-            if (executeCompanionCommand("setup_gpu_spoof")) {
-                LOGI("GPU spoof setup successful");
-                api->setOption(zygisk::Option::FORCE_DENYLIST_UNMOUNT);
-            } else {
-                LOGE("Failed to setup GPU spoof");
-                api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
+            int fd = api->connectCompanion();
+            if (fd >= 0) {
+                const char* cmd = "setup_gpu_spoof";
+                write(fd, cmd, strlen(cmd) + 1);
+                
+                int result = -1;
+                read(fd, &result, sizeof(result));
+                close(fd);
+                
+                if (result == 0) {
+                    LOGI("GPU spoof setup successful");
+                    api->setOption(zygisk::Option::FORCE_DENYLIST_UNMOUNT);
+                } else {
+                    api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
+                }
             }
         } else {
             api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
         }
+        
+        if (package_name) {
+            env->ReleaseStringUTFChars(args->nice_name, package_name);
+        }
     }
-
+    
     void postAppSpecialize(const zygisk::AppSpecializeArgs* args) override {
         api->setOption(zygisk::DLCLOSE_MODULE_LIBRARY);
     }
@@ -59,22 +59,6 @@ public:
 private:
     zygisk::Api* api;
     JNIEnv* env;
-    
-    bool executeCompanionCommand(const std::string& command) {
-        int fd = api->connectCompanion();
-        if (fd < 0) {
-            LOGE("Failed to connect to companion");
-            return false;
-        }
-        
-        write(fd, command.c_str(), command.length() + 1);
-        
-        int result = -1;
-        read(fd, &result, sizeof(result));
-        close(fd);
-        
-        return result == 0;
-    }
 };
 
 REGISTER_ZYGISK_MODULE(GPU830Module)
