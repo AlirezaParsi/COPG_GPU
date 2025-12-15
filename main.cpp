74c91c0ea176ebb81c49ea255300d1c059b30de8
Aliza <alirezanajmabadi79@gmail.com>
2025-12-15 16:09:08 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <jni.h>
 #include <string>
+#include <unistd.h>  // اضافه کردن این هدر
 #include "zygisk.hpp"
 #include <android/log.h>
 
 #define LOG_TAG "GPU830Spoof"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 static const char* TARGET_PACKAGE = "flar2.devcheck";
 
@@ -13,6 +15,7 @@ public:
     void onLoad(zygisk::Api* api, JNIEnv* env) override {
         this->api = api;
         this->env = env;
+        LOGI("GPU 830 Spoof Module loaded");
     }
 
     void preAppSpecialize(zygisk::AppSpecializeArgs* args) override {
@@ -22,9 +25,15 @@ public:
         }
         
         const char* package_name = env->GetStringUTFChars(args->nice_name, nullptr);
+        bool should_spoof = false;
         
-        if (package_name && strcmp(package_name, TARGET_PACKAGE) == 0) {
-            LOGI("GPU 830 spoof activated for: %s", package_name);
+        if (package_name) {
+            LOGI("Checking package: %s", package_name);
+            should_spoof = (strcmp(package_name, TARGET_PACKAGE) == 0);
+        }
+        
+        if (should_spoof) {
+            LOGI("GPU 830 spoof activated!");
             
             // اجرای اسپوف از طریق companion
             int fd = api->connectCompanion();
@@ -40,8 +49,12 @@ public:
                     LOGI("GPU spoof setup successful");
                     api->setOption(zygisk::Option::FORCE_DENYLIST_UNMOUNT);
                 } else {
+                    LOGE("GPU spoof setup failed");
                     api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
                 }
+            } else {
+                LOGE("Failed to connect to companion");
+                api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
             }
         } else {
             api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
